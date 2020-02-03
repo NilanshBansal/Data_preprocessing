0e3c4a829fe2a4eb3af0845b890d795e9990ee54
Nilansh Bansal <bansalnilansh@gmail.com>
2020-02-04 00:05:14 +0530

    scripts modified

--- negate.py
+++ negate.py
@@ -1,4 +1,6 @@
 import nltk
+import re
+nltk.data.path.append('nltk_data')
 from nltk.corpus import wordnet
 # from nltk.tokenize import word_tokenize
 
@@ -15,30 +17,45 @@ class AntonymReplacer(object):
         else:
             return None
 
+    def cleantext(self, text):
+        text = text.lower()
+        text = re.sub(r"i'm", "i am", text)
+        text = re.sub(r"he's", "he is", text)
+        text = re.sub(r"she's", "she is", text)
+        text = re.sub(r"that's", "that is", text)
+        text = re.sub(r"what's", "what is", text)
+        text = re.sub(r"where's", "where is", text)
+        text = re.sub(r"what's", "what is", text)
+        text = re.sub(r"\'ll", " will", text)
+        text = re.sub(r"\'ve", " have", text)
+        text = re.sub(r"\'d", " would", text)
+        text = re.sub(r"n't", "not", text)
+        return text
+
     
     def negreplace(self, string):
         i = 0
         # sentence = word_tokenize(string)
         sentence = string.split(' ')
-        print(sentence)
         len_sent = len(sentence)
-        words = []
+        neg_sentence = ""
         while i < len_sent:
             word = sentence[i]
             if word == 'not' and i+1 < len_sent:
                 ant = self.replace(sentence[i+1])
-                print(ant)
                 if ant:
-                    words.append(ant)
+                    neg_sentence += ant+" "
                     i += 2
                     continue
-            words.append(word)
+            neg_sentence += word+" "
             i += 1
-
-        return words
+        neg_sentence = neg_sentence.strip()
+        return neg_sentence
 
 
 if __name__ == "__main__":
     replacer = AntonymReplacer()
-    sentence = replacer.negreplace('this is not good')
+    text = "I do n't love c++"
+    clean_text = replacer.cleantext(text)
+    sentence = replacer.negreplace(clean_text)
     print(sentence)
--- preproc.py
+++ preproc.py
@@ -1,5 +1,5 @@
 import re,string
-from nltk.corpus import stopwords 
+from nltk.corpus import stopwords, wordnet 
 from nltk.tokenize import word_tokenize 
 
 stop_words = stopwords.words('english')
@@ -20,8 +20,8 @@ def elim_stopwords(sentence):
             tokens = left + joined + right
             i_offset += 1
 
-    # filtered_sentence = [w for w in tokens if not w in stop_words] 
-    filtered_sentence = [w for w in tokens if not w in ['RT','Re']] 
+    filtered_sentence = [w for w in tokens if not w in stop_words] 
+    # filtered_sentence = [w for w in tokens if not w in ['RT','Re']] 
     filtered_sentence = ' '.join(filtered_sentence)
     return filtered_sentence
 
@@ -45,19 +45,65 @@ def strip_all_entities(text):
                 words.append(word)
     return ' '.join(words)
 
-# Cleansing Data -> Removing @, #, RT, Re, links, Html data
+def replace(word):
+    antonyms = set()
+    for syn in wordnet.synsets(word):
+        for lemma in syn.lemmas():
+            for antonym in lemma.antonyms():
+                antonyms.add(antonym.name())
+    if len(antonyms) >= 1:
+        return antonyms.pop()
+    else:
+        return None
+
+def cleantext(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"n't", "not", text)
+    return text
+
+
+def negreplace(string):
+    i = 0
+    # sentence = word_tokenize(string)
+    sentence = string.split(' ')
+    len_sent = len(sentence)
+    neg_sentence = ""
+    while i < len_sent:
+        word = sentence[i]
+        if word == 'not' and i+1 < len_sent:
+            ant = replace(sentence[i+1])
+            if ant:
+                neg_sentence += ant+" "
+                i += 2
+                continue
+        neg_sentence += word+" "
+        i += 1
+    neg_sentence = neg_sentence.strip()
+    return neg_sentence
+
+
 
 tests = [
     "I am at Starbucks http://4sh.com/samqUI (7419 3rd ave, at 75th, Brooklyn)",
-    "I c RT @iamFink: @SamanthaSpice that's my excited face and my regular face. The expression never changes.",
-    "RT @AstrologyForYou: #Gemini recharges through regular contact with people of like mind, and social involvement that allows expression of their ideas",
+    "I c RT @iamFink: @SamanthaSpice that's my excited face and my regular face.",
+    "RT @AstrologyForYou: #Gemini recharges through regular contact with people of like mind.",
     "New comment by diego.bosca: Re: Re: wrong regular expression? http://t.co/4KOb94ua",
     
     "@lakatos88 Python framework wasn't good.",
     "@peter I really love that shirt at #Macy. http://bet.ly//WjdiW4",
     "I love C++",
-    "I love C#",
-    "Do I love C#?",
+    "I do not love c++",
+    "I don't love c++",
 ]
 
 # tests = [
@@ -67,5 +113,6 @@ tests = [
 
 
 for t in tests:
-    print(elim_stopwords(strip_all_entities(strip_links(t))))
+    # print(elim_stopwords(strip_all_entities(strip_links(t))))
     # print(strip_all_entities(strip_links(t)))
+    print(negreplace(cleantext(elim_stopwords(strip_all_entities(strip_links(t))))))
